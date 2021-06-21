2789f498cfcf790d745e1e06d66a03f2e0c2a81f
Shinnosuke🍮 Hashimoto <hashimoto@Shinnosuke-no-Mac-mini.local>
2021-06-21 22:10:37 +0900

    first commit

---
+++ public/auth.js
@@ -0,0 +1,48 @@
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+var uiConfig = {
+    callbacks: {
+      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+        // User successfully signed in.
+        // Return type determines whether we continue the redirect automatically
+        // or whether we leave that to developer to handle.        
+        return true
+      },
+      uiShown: function() {
+        // The widget is rendered.
+        // Hide the loader.
+        // document.getElementById('loader').style.display = 'none';        
+      }
+    },
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    signInFlow: 'popup',
+    signInSuccessUrl: './index.html',
+    signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+      // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      // firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      // firebase.auth.PhoneAuthProvider.PROVIDER_ID
+    ],
+    // Terms of service url.
+    tosUrl: '<your-tos-url>',
+    // Privacy policy url.
+    privacyPolicyUrl: '<your-privacy-policy-url>'
+};
+
+// firebase.auth().signOut()
+
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        // console.log(user.uid);
+        var uid = user.uid;
+        // document.getElementById('firebaseui-auth-container').style.display = 'none';
+    }
+    else {
+        document.getElementById('form').style.display = 'none';
+        ui.start('#firebaseui-auth-container', uiConfig);        
+    }
+})
